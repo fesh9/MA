f82932dc6c86b117600013b644b76ec7acf35b3e
fesh9 <dr.sawckov@yandex.ru>
2026-02-27 19:01:27 +0700

    Test webhook

--- lab2/service-a/server.js
+++ lab2/service-a/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Hello from Service A on port ${PORT}`);
+  res.send(`Hello from Service A - CI/CD is working!`);
 });
 
 app.listen(PORT, () => console.log(`Service A running on port ${PORT}`));
