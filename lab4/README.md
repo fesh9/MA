b4780ee25de7f323c08fb221505ab6deb977c27d
fesh9 <dr.sawckov@yandex.ru>
2026-02-27 18:37:59 +0700

    Keep only lab2 files in this branch

--- lab4/README.md
+++
@@ -1,3 +0,0 @@
-# curl -v http://localhost/service1/info
-
-curl http://localhost/service1/hello/John
--- lab5/service_6/service_6.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3001;
-app.get('/', (req, res) => {
-    res.send(`Hello from Service 6 (Cluster Mode) on port ${PORT}`);
-});
-app.listen(PORT, () => console.log(`Service 6 running on port ${PORT}`));
